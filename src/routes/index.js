a8797fdf07e764fa0f378e01fec425d0f71195d2
unknown <domirandar@unal.edu.co>
2023-06-06 12:33:41 -0500

    First commit(Boilerplate created. %s listening at 3001,db arts conected)

---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+const { Router } = require('express');
+const router = Router();
+
+// Setting up routers first it must be defined and import his file
+// router.use('/artworks', artworksRouter);
+
+module.exports = router;
