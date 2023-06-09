9fe673f3b6e03f13ae15dbb1bed36d993bfbc6ce
Ssamza <santiagoemezeta@gmail.com>
2023-06-09 10:26:57 -0500

    feat(add new route: artworksRouter, modified index & userRouter)

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,12 +1,7 @@
 const { Router } = require("express");
 const usersRouter = Router();
-const {
-  postUsersHandler,
-  getArtworkHandler,
-} = require("../handlers/postUsersHandler.js");
+const { postUsersHandler } = require("../handlers/postUsersHandler.js");
 
 usersRouter.post("/", postUsersHandler);
-
-usersRouter.get("/", getArtworkHandler);
 
 module.exports = usersRouter;
