16faece4e1502086928ba05fcd4b4baa75de926f
Ssamza <santiagoemezeta@gmail.com>
2023-06-09 10:51:56 -0500

    feat(add search artwork by name (query))

--- src/handlers/artworkHandler.js
+++ src/handlers/artworkHandler.js
@@ -2,7 +2,17 @@ const getArtwork = require("../controllers/usersControllers.js");
 
 const getArtworkHandler = async (req, res) => {
   try {
+    const { name } = req.body;
     const response = getArtwork();
+    if (name) {
+      let artworkName = response.filter((works) => {
+        works.title.toLowerCase().includes(name.toLowerCase());
+      });
+      if (artworkName === null) {
+        throw Error("Artwork not available");
+      }
+      res.status(200).json(artworkName);
+    }
     res.status(200).json(response);
   } catch (error) {
     res.status(422).json({ error: error.message });
