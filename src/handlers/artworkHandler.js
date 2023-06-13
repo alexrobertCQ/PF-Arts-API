afc4eb5df4cd317b80ec974bd71d17738f03a65a
Ssamza <santiagoemezeta@gmail.com>
2023-06-12 20:05:51 -0500

    feat(route/handler/controller POST method to create Artworks)

--- src/handlers/artworkHandler.js
+++ src/handlers/artworkHandler.js
@@ -1,25 +1,47 @@
-const getArtwork = require("../controllers/artworkController.js");
+const {
+  getAllArtwork,
+  createArtwork,
+} = require('../controllers/artworkController.js');
 
 const getArtworkHandler = async (req, res) => {
   try {
     const { title } = req.query;
-    const response = await getArtwork();
+    const response = await getAllArtwork();
     if (title) {
       let artworkName = response.filter((works) => {
         return works.title.toLowerCase().includes(title.toLowerCase());
       });
       if (artworkName.length === 0) {
-        throw Error("Artwork not available");
+        throw Error('Artwork not available');
       }
       res.status(200).json(artworkName);
-    }else{
-    res.status(200).json(response)
-   }
+    } else {
+      res.status(200).json(response);
+    }
   } catch (error) {
     res.status(422).json({ error: error.message });
   }
 };
 
+const postArtworkHandler = async (req, res) => {
+  const { title, artistName, image, completitionYear } = req.body;
+  try {
+    if (!title || !artistName || !image || !completitionYear) {
+      throw Error('Missing data');
+    }
+    const response = await createArtwork(
+      title,
+      artistName,
+      image,
+      completitionYear
+    );
+    res.status(201).json(response);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
 module.exports = {
   getArtworkHandler,
+  postArtworkHandler,
 };
