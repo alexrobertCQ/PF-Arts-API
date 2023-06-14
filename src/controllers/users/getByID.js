5dd3ecc465941aae9f1a3e41a718b893401c272b
Ssamza <santiagoemezeta@gmail.com>
2023-06-14 12:45:29 -0500

    feat(updated createUser controller. Created getByID controller)

---
+++ src/controllers/users/getByID.js
@@ -0,0 +1,20 @@
+const { User, Artwork } = require('../db.js');
+const getUserID = async (id) => {
+  const empty = await User.count();
+  if (empty === 0) {
+    throw Error('No info available');
+  } else {
+    const userByID = await User.findByPk(id, {
+      include: {
+        model: Artwork,
+        attributes: ['title', 'image'],
+      },
+    });
+    if (!userByID) {
+      throw new Error('No match found');
+    }
+    return userByID;
+  }
+};
+
+module.exports = getUserID;
