64726836cf78047818bfa8188d532a6914f2117f
David Orlando Miranda Roa <domirandar@unal.edu.co>
2023-06-14 10:56:11 -0500

    Fix(Modularize controllers and handlers)

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 const usersRouter = Router();
-const {
+const postUsersHandler = require ('../handlers/users/postHandler');
+const getUsersHandler = require ('../handlers/users/getHandler.js');
+/* const {
   postUsersHandler,
   getUsersHandler,
-} = require('../handlers/usersHandler.js');
+} = require('../handlers/usersHandler.js'); */
 
 usersRouter.post('/', postUsersHandler);
 
