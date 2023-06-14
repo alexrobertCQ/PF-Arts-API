faf67a951db6b9712f1296241d48209a3838eb53
Ssamza <santiagoemezeta@gmail.com>
2023-06-14 12:47:23 -0500

    feat(created newHandlers getByID for user & artwork)

---
+++ src/handlers/artworks/getByIDHandler.js
@@ -0,0 +1,13 @@
+const getArtID = require('../../controllers/artworks/getByIDController');
+
+const getArtIdHandler = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const response = await getArtID(id);
+    res.status(200).json(response);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+module.exports = getArtIdHandler;
