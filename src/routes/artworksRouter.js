b58bd761a4678458636624eb964ae8b18f450453
David Orlando Miranda Roa <domirandar@unal.edu.co>
2023-06-15 17:47:02 -0500

    Feature(API/new route PUT artworks)

--- src/routes/artworksRouter.js
+++ src/routes/artworksRouter.js
@@ -5,6 +5,7 @@ const getArtworkHandler = require('../handlers/artworks/getHandler');
 const getArtIdHandler = require('../handlers/artworks/getByIDHandler');
 const postArtworkHandler = require('../handlers/artworks/postHandler');
 const artworksPagingHandler = require('../handlers/artworks/pagingHandler');
+const putArtworkHandler = require('../handlers/artworks/putArtworkHandler')
 
 artworksRouter.get('/', getArtworkHandler);
 
@@ -13,5 +14,7 @@ artworksRouter.get('/detail/:id', getArtIdHandler);
 artworksRouter.get('/db', artworksPagingHandler);
 
 artworksRouter.post('/', postArtworkHandler);
+
+artworksRouter.put('/:id', putArtworkHandler);
 
 module.exports = artworksRouter;
