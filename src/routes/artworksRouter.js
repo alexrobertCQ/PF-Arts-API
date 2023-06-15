0cfe17c4047e7f7501cf4abbb48d5a7013684c3c
Ssamza <santiagoemezeta@gmail.com>
2023-06-15 18:03:13 -0500

    feat(2 new routes for Artwork, delete and put)

--- src/routes/artworksRouter.js
+++ src/routes/artworksRouter.js
@@ -5,7 +5,8 @@ const getArtworkHandler = require('../handlers/artworks/getHandler');
 const getArtIdHandler = require('../handlers/artworks/getByIDHandler');
 const postArtworkHandler = require('../handlers/artworks/postHandler');
 const artworksPagingHandler = require('../handlers/artworks/pagingHandler');
-const putArtworkHandler = require('../handlers/artworks/putArtworkHandler')
+const putArtworkHandler = require('../handlers/artworks/putArtworkHandler');
+const deleteArtworkHandler = require('../handlers/artworks/deleteHandler');
 
 artworksRouter.get('/', getArtworkHandler);
 
@@ -16,5 +17,7 @@ artworksRouter.get('/db', artworksPagingHandler);
 artworksRouter.post('/', postArtworkHandler);
 
 artworksRouter.put('/:id', putArtworkHandler);
+
+artworksRouter.delete('/:id', deleteArtworkHandler);
 
 module.exports = artworksRouter;
