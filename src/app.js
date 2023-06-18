4ba0d712804a6bf6ef8458f310804eb2b5e291c0
Ssamza <santiagoemezeta@gmail.com>
2023-06-18 08:04:11 -0500

    feat(cambios menores para conectar back-front localmente)

--- src/app.js
+++ src/app.js
@@ -1,38 +1,38 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-const routes = require("./routes/index.js");
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const routes = require('./routes/index.js');
 
-require("./db.js");
+require('./db.js');
 
 const server = express();
 server.use(express.json());
 
-server.name = "API";
+server.name = 'API';
 // Using middlewares.
-server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-server.use(bodyParser.json({ limit: "50mb" }));
+server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(
   morgan(
-    "Method: :method URL: :url Status: :status :response-time ms - :res[content-length] kilobytes - :date[clf]."
+    'Method: :method URL: :url Status: :status :response-time ms - :res[content-length] kilobytes - :date[clf].'
   )
 );
 // Setting CORS
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-  //res.header("Access-Control-Allow-Credentials", "true");
+  res.header('Access-Control-Allow-Origin', '*');
+  // res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  // res.header("Access-Control-Allow-Credentials", "true");
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
   );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
 // Routing
-server.use("/", routes);
+server.use('/', routes);
 
 // Error catching endware.
 server.use((err, req, res, next) => {
