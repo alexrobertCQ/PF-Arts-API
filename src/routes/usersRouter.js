7601deea71f0dcd75f72251f0f8fa11d2a34a26b
David Orlando Miranda Roa <domirandar@unal.edu.co>
2023-06-22 13:48:40 -0500

    Fix/Feature(API/utils API/usersRoute) route users edit created, crud completed. using token middleware

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -3,11 +3,14 @@ const usersRouter = Router();
 const postUsersHandler = require('../handlers/users/postHandler');
 const getUsersHandler = require('../handlers/users/getHandler.js');
 const getUserIdHandler = require('../handlers/users/getByIDHandler');
+const loginHandler = require('../handlers/users/loginHandler');
+const updateUsersHandler = require('../handlers/users/updateHandler');
+const authenticateToken = require('../utils/authenticateToken'); // Importa el middleware
 
 usersRouter.post('/', postUsersHandler);
-
 usersRouter.get('/', getUsersHandler);
-
 usersRouter.get('/detail/:id', getUserIdHandler);
+usersRouter.put('/edit', authenticateToken, updateUsersHandler);
+usersRouter.post('/login', loginHandler);
 
 module.exports = usersRouter;
