31db613f9c79678f599f4d9a8ae29b242168136e
David Orlando Miranda Roa <domirandar@unal.edu.co>
2023-06-23 15:19:29 -0500

    Refactor(API/users)users/ edit handler and controller refactor and identated

--- src/handlers/users/updateHandler.js
+++ src/handlers/users/updateHandler.js
@@ -1,12 +1,21 @@
-const updateUser = require('../../controllers/users/updateUser');
+const updateUser = require("../../controllers/users/updateUser");
 
 const updateUsersHandler = async (req, res) => {
   const id = req.userId;
-  console.log(id);
-  const { userName, description, email, password, phoneNumber, location } = req.body;
-  const profilePicture = typeof req.file === 'object' ? req.file.path : req.body.profilePicture;
+  const { userName, description, password, phoneNumber, location } =
+    req.body;
+  const profilePicture =
+    typeof req.file === "object" ? req.file.path : req.body.profilePicture;
   try {
-    const updatedUser = await updateUser(id, userName, profilePicture, description, email, password, phoneNumber, location);
+    const updatedUser = await updateUser(
+      id,
+      userName,
+      profilePicture,
+      description,
+      password,
+      phoneNumber,
+      location
+    );
     res.status(200).json(updatedUser);
   } catch (error) {
     res.status(422).json({ error: error.message });
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -7,7 +7,7 @@ const getUsersHandler = require('../handlers/users/getHandler.js');
 const getUserIdHandler = require('../handlers/users/getByIDHandler');
 const loginHandler = require('../handlers/users/loginHandler');
 const updateUsersHandler = require('../handlers/users/updateHandler');
-const authenticateToken = require('../utils/authenticateToken'); // Importa el middleware
+const authenticateToken = require('../utils/authenticateToken');
 const getFavoritesHandler = require('../handlers/users/getFavoritesHandler');
 
 usersRouter.post('/', postUsersHandler);
