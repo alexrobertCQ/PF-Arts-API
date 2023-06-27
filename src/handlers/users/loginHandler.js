1c65b821ac366a68a69c5d328eea4b7881ba75cd
JhonDaniel01 <jhon-coso@hotmail.com>
2023-06-27 01:19:52 -0500

    Modificacion de filtrado

--- src/handlers/users/loginHandler.js
+++ src/handlers/users/loginHandler.js
@@ -2,7 +2,7 @@ const validateUser = require('../../controllers/users/loginController');
 
 const loginHandler = async (req, res) => {
   try {
-    const { username, password } = req.body;
+    const { username, password } = req.body;//Aqui estaba teniendo problemas no deberia ser userName?
     const token = await validateUser(username, password);
     res.status(200).json(
       {token,
